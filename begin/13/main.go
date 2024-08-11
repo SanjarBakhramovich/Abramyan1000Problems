e50bf66816b74c1f2a6acd839bef1c8c8970536e
Sanjar <sanjarbakhramovich@gmail.com>
2024-08-11 15:43:30 +0500

    exc 13 done

--- begin/13/main.go
+++ begin/13/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2 ).
 // Найти площади этих кругов S 1 и S 2 ,
@@ -16,10 +19,22 @@ func findCircArea(radius float64) float64{
 	return math.Pi * math.Pow(radius, 2)
 }
 
-func findOuterRadius()
-
-func findInnerRadius()
+func findRingArea(outerRadius, innerRadius float64) float64{
+	areaOuter := findCircArea(outerRadius)
+	areaInner := findCircArea(innerRadius)
+	return areaOuter - areaInner
+}
 
 func main() {
-	
+	R1 := 5.0
+	R2 := 3.0
+
+	S1 := findCircArea(R1)
+	S2 := findCircArea(R2)
+	// 
+	S3 := findRingArea(R1, R2)
+	// 
+	fmt.Printf("Площадь круга с радиусом %.2f: %.2f\n", R1, S1)
+	fmt.Printf("Площадь второго круга с радиусом %.2f: %.2f\n", R2, S2)
+	fmt.Printf("Площадь кольца %.2f\n", S3)
 }
